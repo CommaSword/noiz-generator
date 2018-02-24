edd6ad9679247f9dba42f00f6c49eb7b7d601ae1
Aram Drevekenin <grimsniffer@gmail.com>
2018-02-24 01:59:24 +0100

    refactor: index file

--- index.js
+++ index.js
@@ -7,24 +7,29 @@ function isAudioFile (filename) {
   return /\.mp3$/.test(filename)
 }
 
+function audioFileLoader (dir, Audio) {
+  return async function loadAudioFile (filename) {
+    const aud = new Audio(`${dir}/${filename}`)
+    try {
+      await aud.play()
+      await aud.loop(true)
+      return aud
+    } catch (e) {
+      throw new Error('timed out waiting for electron process')
+    }
+  }
+}
+
 module.exports = {
   async noiseGenerator (dir) {
     const Audio = createAudio()
     const files = fs.readdirSync(dir)
     const audioFileNames = files.filter(isAudioFile)
+    const loadAudioFile = audioFileLoader(dir, Audio)
     const audioFiles = await Promise.all(
-      audioFileNames.map(async filename => {
-        const aud = new Audio(`${dir}/${filename}`)
-        try {
-          await aud.play()
-          await aud.loop(true)
-          return aud
-        } catch (e) {
-          throw new Error('timed out waiting for electron process')
-        }
-      })
+      audioFileNames.map(loadAudioFile)
     )
-    return (...volumes) => {
+    return function noise (...volumes) {
       if (volumes.length !== audioFiles.length) {
         throw new Error(
           `invalid argument length, received ${volumes.length} but have ${audioFiles.length} files loaded`
