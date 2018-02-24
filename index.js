c10797779c50510247f2d21188c96366e4616bce
Aram Drevekenin <grimsniffer@gmail.com>
2018-02-24 01:37:34 +0100

    feat: async api

--- index.js
+++ index.js
@@ -4,29 +4,38 @@ const fs = require('fs')
 const createAudio = require('./lib/node-audio-element.js')
 
 function isAudioFile (filename) {
-  // TODO: properly
   return /\.mp3$/.test(filename)
 }
 
 module.exports = {
-  noiseGenerator (dir) {
+  async noiseGenerator (dir) {
     const Audio = createAudio()
     const files = fs.readdirSync(dir)
     const audioFileNames = files.filter(isAudioFile)
-    const audioFiles = audioFileNames.map(filename => {
-      const aud = new Audio(`${dir}/${filename}`)
-      aud.play()
-      aud.loop = true
-      return aud
-    })
+    const audioFiles = await Promise.all(
+      audioFileNames.map(async filename => {
+        const aud = new Audio(`${dir}/${filename}`)
+        try {
+          await aud.play()
+          await aud.loop(true)
+          return aud
+        } catch (e) {
+          throw new Error('timed out waiting for electron process')
+        }
+      })
+    )
     return (...volumes) => {
       if (volumes.length !== audioFiles.length) {
         throw new Error(
           `invalid argument length, received ${volumes.length} but have ${audioFiles.length} files loaded`
         )
       }
-      volumes.forEach((value, i) => {
-        audioFiles[i].volume = value
+      return Promise.all(
+        volumes.map((value, i) => {
+          return audioFiles[i].volume(value)
+        })
+      ).catch((e) => {
+        throw new Error('timed out waiting for electron process')
       })
     }
   }
